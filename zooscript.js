d9b36d66743ea0b47921942415cd3da3ab24fdf6
Ib Kunz Mathiesen <ikm@levacom.dk>
2025-09-17 11:31:14 +0200

    Add js post function

--- zooscript.js
+++ zooscript.js
@@ -13,4 +13,27 @@ async function getData() {
     }
 }
 
+async function postData(id, animal) {
+    const url = "http://localhost:5257/zoo";
+    
+    try 
+    {
+        const response = await fetch(url, 
+            {
+            method: "POST",
+            headers: 
+                {
+                    "Content-Type": "application/json"
+                },
+            body: JSON.stringify({id: id, animal: animal})
+        });
+        console.log(response.status);
+    } catch (error) 
+    {
+            console.log(error);
+    }
+}
+
+await getData();
+await postData(5, "Cow");
 await getData();
